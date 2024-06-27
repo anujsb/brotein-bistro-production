dc36f7e4a73ebc6546d2007bf8acaeacd4073dac
Shwetas Dhake <shwetas.dhake@mitaoe.ac.in>
2024-06-27 12:46:37 +0530

    admin ui done

--- client/src/components/SideBarAdmin.jsx
+++ client/src/components/SideBarAdmin.jsx
@@ -28,7 +28,7 @@ const SideBarAdmin = () => {
 
   return (
     <nav
-      className={`md:h-screen lg:h-screen sm:h-min sm:w-full md:w-fit lg:w-fit transition-all duration-300 flex md:flex-col lg:flex-col sm:flex-row sticky  left-0 top-0 p-2  pt-24 rounded-sm shadow-md border bg-white  ${
+      className={`md:h-screen lg:h-screen sm:h-min sm:w-full md:w-fit lg:w-fit transition-all duration-300 flex md:flex-col lg:flex-col sm:flex-row  sticky  left-0 top-0 p-2  pt-24 rounded-sm shadow-md border bg-white  ${
         expanded ? "sm:w-full md:w-56 lg:w-52" : ""
       }`}
     >
@@ -38,14 +38,14 @@ const SideBarAdmin = () => {
       >
         <BsLayoutSidebarInset size={18} fill="#444" />
       </div>
-      <ul className="flex md:flex-col lg:flex-col sm:flex-row w-full ">
+      <ul className="flex md:flex-col lg:flex-col sm:flex-row w-full  justify-center">
         <li>
           <Link
             to="/subscriptionse-requests"
             className={`${getLinkClasses("/subscriptionse-requests")} flex `}
           >
             <img alt="requests" className="w-6 mr-4" src="/request.svg" />
-            {expanded && showText && <p className="duration-300">Requests</p>}
+            {expanded && showText && <p className="hidden sm:block">Requests</p>}
           </Link>
         </li>
         <li>
@@ -54,7 +54,7 @@ const SideBarAdmin = () => {
             className={`${getLinkClasses("/active-subscriptions")} flex`}
           >
             <img alt="requests" className="w-6 mr-4" src="/active.svg" />
-            {expanded && showText && <p>Active</p>}
+            {expanded && showText && <p className="hidden sm:block">Active</p>}
           </Link>
         </li>
         <li>
@@ -63,7 +63,7 @@ const SideBarAdmin = () => {
             className={`${getLinkClasses("/meal-records")} flex`}
           >
             <img alt="requests" className="w-6 mr-4" src="/records.svg" />
-            {expanded && showText && <p>Records</p>}
+            {expanded && showText && <p className="hidden sm:block">Records</p>}
           </Link>
         </li>
         <li>
@@ -72,7 +72,7 @@ const SideBarAdmin = () => {
             className={`${getLinkClasses("/upload-add")} flex`}
           >
             <img alt="requests" className="w-6 mr-4" src="/records.svg" />
-            {expanded && showText && <p className="">Upload Add</p>}
+            {expanded && showText && <p className="hidden sm:block">Upload Add</p>}
           </Link>
         </li>
         <li>
@@ -81,7 +81,7 @@ const SideBarAdmin = () => {
             className={`${getLinkClasses("/feedback-responses")} flex`}
           >
             <img alt="requests" className="w-6 mr-4" src="/active.svg" />
-            {expanded && showText && <p>feedback</p>}
+            {expanded && showText && <p className="hidden sm:block">feedback</p>}
           </Link>
         </li>
       </ul>
